de84093365d1e0db3a43c26c39631035f697e002
Christian Kim <chris.kim320@gmail.com>
2025-01-14 16:21:46 -0500

    add google maps api and create search parks function

--- backend/controllers/game.controller.js
+++ backend/controllers/game.controller.js
@@ -1,4 +1,5 @@
 import Game from '../models/game.model.js';
+import axios from 'axios';
 import { protect } from '../middleware/authMiddleware.js';
 
 export const createGame = async (req, res) => {
@@ -22,3 +23,27 @@ export const createGame = async (req, res) => {
         res.status(500).json({ message: 'Serer error. Please try again later.' });
     }
 };
+
+export const searchPark = async (req, res) => {
+    const { city } = req.query;
+
+    if (!city) {
+        return res.status(400).json({ message: 'City is required to fetch parks' });
+    }
+
+    try {
+        const API_KEY = process.env.MAPS_API_KEY;
+        const response = await axios.get(
+            `https://maps.googleapis.com/maps/api/place/textsearch/json?query=parks+in+${city}&key=${API_KEY}`
+        );
+
+        const parks = response.data.results.map((park) => ({
+            name: park.name,
+            address: park.formatted_address,
+        }));
+        res.status(200).json({ parks });
+    } catch (error) {
+        console.error('Error fetching parks:', error.message);
+        res.status(500).json({ message: 'Error fetching parks. Please try again.' });
+    }
+}
--- backend/routes/game.routes.js
+++ backend/routes/game.routes.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { protect } from '../middleware/authMiddleware.js';
-import { createGame } from '../controllers/game.controller.js';
+import { createGame, searchPark } from '../controllers/game.controller.js';
 
 const router = express.Router();
 
 router.post('/create', protect, createGame);
+
+router.get('/parks', protect, searchPark);
 
 export default router;
